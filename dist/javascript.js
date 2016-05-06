a2af632b6ec09e73978519a4c767d12b5f13c146
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-05-06 11:19:11 -0300

    código js melhorado

--- dist/javascript.js
+++ dist/javascript.js
@@ -4,34 +4,15 @@ var nome;
 	function assyncRequest(){
 		i = document.getElementById("chave").value;
 		nome = document.getElementById("chavenome").value;
-		console.log(nome);
-		
-		if (nome == ""){
+
 			var xmlhttp = new XMLHttpRequest();
-			var url ="http://192.168.1.109:8080/product?chave="+i;
 			
-			xmlhttp.onreadystatechange = function (){
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-					var myArr = JSON.parse(xmlhttp.responseText);
-					estoque(myArr);
-				}
-			}; 
-			xmlhttp.open("GET", url, true);
-			xmlhttp.send();
-		
-			function estoque(produtos){
-				var out = "";
-				out+= 'Nome: ' + produtos.nome + '<br />';
-				out+='Valor: ' + produtos.valor + '<br />';
-				out+='Status: ' + produtos.status + '<br />';
-				out+='Estoque: ' + produtos.estoque + '<br />';
-				document.getElementById("conteudo").innerHTML = out;
+			if (i == "") {
+				var url ="http://192.168.1.109:8080/product?nome="+nome;
 			}
-		}
-		
-		if (i == ""){
-			var xmlhttp = new XMLHttpRequest();
-			var url ="http://192.168.1.109:8080/product?nome="+nome;
+			if (nome == ""){
+				var url ="http://192.168.1.109:8080/product?chave="+i;
+			}
 			
 			xmlhttp.onreadystatechange = function (){
 				if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
@@ -51,4 +32,3 @@ var nome;
 				document.getElementById("conteudo").innerHTML = out;
 			}
 		}	
-	}
