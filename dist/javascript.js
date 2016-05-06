1157c038fb5a1062a27466561de2c765f47e43df
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-05-06 15:56:02 -0300

    redimensionado

--- dist/javascript.js
+++ dist/javascript.js
@@ -1,11 +1,15 @@
 var i;
+var check;
+var listatodos;
 
-	
 	function assyncRequest(){
 		i = document.getElementById("chave").value;
+		check = document.getElementById("listatodos").value;
+		
+		console.log(check);
+		console.log(listatodos);
 		var valor = isNaN(i);
 			var xmlhttp = new XMLHttpRequest();
-			
 			
 			if (valor == true) {
 				var url ="http://192.168.1.109:8080/product?nome="+i;
@@ -15,7 +19,11 @@ var i;
 				var url ="http://192.168.1.109:8080/product?chave="+i;
 			}
 			
-			console.log(valor);
+			if (check == true){
+				var url ="http://192.168.1.109:8080/list";
+				listatodos = 1;
+			}
+			
 			xmlhttp.onreadystatechange = function (){
 				if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
 					var myArr = JSON.parse(xmlhttp.responseText);
@@ -25,12 +33,37 @@ var i;
 			xmlhttp.open("GET", url, true);
 			xmlhttp.send();
 		
-			function estoque(produtos){
+		
+			if (listatodos == 1){
+				function estoque(produtos){
+					var x;
+					for (x = 0; x < produtos.length; x++){
+						var out = "";
+						out+='<table border="1"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+						out+= '<tr><td>' + produtos[x].nome + '</td>';
+						out+='<td>' + produtos[x].valor + '</td>';
+						out+='<td>' + produtos[x].status + '</td>';
+						out+='<td>' + produtos[x].estoque + '</td></tr></table>';
+						document.getElementById("conteudo").innerHTML = out;
+					}
+				}
+			}
+			
+		
+			else if (listatodos !=1){
+				function estoque(produtos){
 				var out = "";
-				out+= 'Nome: ' + produtos.nome + '<br />';
-				out+='Valor: ' + produtos.valor + '<br />';
-				out+='Status: ' + produtos.status + '<br />';
-				out+='Estoque: ' + produtos.estoque + '<br />';
+				out+='<table border="1"><tr><th>Chave</th><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+				out+= '<tr><td>' + produtos.chave + '</td>';
+				out+= '<td>' + produtos.nome + '</td>';
+				out+='<td>' + produtos.valor + '</td>';
+				out+='<td>' + produtos.status + '</td>';
+				out+='<td>' + produtos.estoque + '</td></tr></table>';
 				document.getElementById("conteudo").innerHTML = out;
+				}
 			}
 		}	
+		
+	
+
+		
