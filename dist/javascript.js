e61ea9648abd053443cab74ac32bdb8dd985493a
Luísa Antoni da Rocha <luisa.rocha@digitaldesk.com.br>
2016-05-09 15:37:46 -0300

    botao todosprodutos com requisição javascript

--- dist/javascript.js
+++ dist/javascript.js
@@ -1,38 +1,22 @@
-var i;
-var lista;
-var lebotao;
 var url;
-	
-	$( document ).ready(function() {
-		$("#botaotodos").click(function(){
-			$.getJSON("http://192.168.1.109:8080/list", function (data){
-				var i;
-				var out = "";
-				out+='<table border="1"><tr><th>Chave</th><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
-				for (i=0; i < data.length; i++){
-				out+= '<tr><td>' + data[i].chave + '</td>';
-				out+= '<td>' + data[i].nome + '</td>';
-				out+='<td>' + data[i].valor + '</td>';
-				out+='<td>' + data[i].status + '</td>';
-				out+='<td>' + data[i].estoque + '</td></tr>';
-				}
-				'</table>'
-			$("#conteudo").html(out);
-		});
-		});
-	});
+
+	function todos(){
+		lista=2;
+		url= "http://192.168.1.109:8080/list";
+		assyncRequest(lista);
+	}
 	
 	function limpa(){
 		document.getElementById("conteudo").innerHTML = "";
-		document.getElementById("chave").innerHTML = '';
+		/*document.getElementById("chave").innerHTML = '';*/
 	}
 
-	function assyncRequest(){
-		i = document.getElementById("chave").value.toLowerCase();
+	function teste(){
+		var lista;
+		var i = document.getElementById("chave").value.toLowerCase();
 		if (i != ""){
 			var valor = isNaN(i);
-			var xmlhttp = new XMLHttpRequest();
-			
+			lista = 1;
 			if (valor == true) {
 				url ="http://192.168.1.109:8080/product?nome="+i;
 			}
@@ -40,27 +24,48 @@ var url;
 			if (valor == false){
 				url ="http://192.168.1.109:8080/product?chave="+i;
 			}
-			
-			xmlhttp.onreadystatechange = function (){
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-					var myArr = JSON.parse(xmlhttp.responseText);
-						estoque(myArr);
-				}
-			}; 
-			
-			xmlhttp.open("GET", url, true);
-			xmlhttp.send();
-			
-			function estoque(produtos){
-				var out = "";
-				out+='<table border="1"><tr><th>Chave</th><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
-				out+= '<tr><td>' + produtos.chave + '</td>';
-				out+= '<td>' + produtos.nome + '</td>';
-				out+='<td>' + produtos.valor + '</td>';
-				out+='<td>' + produtos.status + '</td>';
-				out+='<td>' + produtos.estoque + '</td></tr></table>';
-				document.getElementById("conteudo").innerHTML = out;			
-			}
+			assyncRequest(lista);
 		}
 	}
+	
+	function assyncRequest(lista){
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function (){
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+				var myArr = JSON.parse(xmlhttp.responseText);
+					estoque(myArr, lista);
+			}
+		}; 
+		
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();		
+	}
+	
+	function estoque(produtos, lista){
+		var out = "";
+		
+		if (lista == 1){
+			out+='<table border="1"><tr><th>Chave</th><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+			out+= '<tr><td>' + produtos.chave + '</td>';
+			out+= '<td>' + produtos.nome + '</td>';
+			out+='<td>' + produtos.valor + '</td>';
+			out+='<td>' + produtos.status + '</td>';
+			out+='<td>' + produtos.estoque + '</td></tr></table>';
+		}
+		
+		if (lista == 2){
+			out+='<table border="1"><tr><th>Chave</th><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
+			for (i=0; i < produtos.length; i++){
+				out+= '<tr><td>' + produtos[i].chave + '</td>';
+				out+= '<td>' + produtos[i].nome + '</td>';
+				out+='<td>' + produtos[i].valor + '</td>';
+				out+='<td>' + produtos[i].status + '</td>';
+				out+='<td>' + produtos[i].estoque + '</td></tr>';
+			}
+			'</table>'
+		}
+		document.getElementById("conteudo").innerHTML = out;			
+	}
+
+
 		
